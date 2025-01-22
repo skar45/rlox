f356afe3450b6a0fddd928f0fd578a17d5a912e8
Sajal <karkee455@gmail.com>
2025-01-21 21:19:21 -0500

    linting

--- src/ast/ast.rs
+++ src/ast/ast.rs
@@ -21,7 +21,7 @@ enum Expr {
     Literal(Literal),
     Binary(Binary),
     Unary(Unary),
-    Grouping(Grouping)
+    Grouping(Grouping),
 }
 
 impl Expr {
@@ -30,34 +30,34 @@ impl Expr {
             Expr::Literal(l) => l.value.to_string(),
             Expr::Binary(b) => parenthize_expr!(&b.operator.lexme, b.left, b.right),
             Expr::Unary(u) => parenthize_expr!(&u.operator.lexme, u.right),
-            Expr::Grouping(g) => parenthize_expr!("group", g.expression)
+            Expr::Grouping(g) => parenthize_expr!("group", g.expression),
         }
     }
 }
 
 struct Literal {
-    value: LiteralType
+    value: LiteralType,
 }
 
 struct Grouping {
-    expression: Box<Expr>
+    expression: Box<Expr>,
 }
 
 struct Binary {
     left: Box<Expr>,
     operator: Token,
-    right: Box<Expr>
+    right: Box<Expr>,
 }
 
 struct Unary {
     operator: Token,
-    right: Box<Expr>
+    right: Box<Expr>,
 }
 
 #[test]
 pub fn liter_expr() {
     let l = Expr::Literal(Literal {
-        value: LiteralType::Num(2.0)
+        value: LiteralType::Num(2.0),
     });
     assert_eq!("2", l.accept());
 }
@@ -65,34 +65,34 @@ pub fn liter_expr() {
 #[test]
 pub fn group_expr() {
     let l1 = Expr::Literal(Literal {
-        value: LiteralType::Num(4.0)
+        value: LiteralType::Num(4.0),
     });
     let l2 = Expr::Literal(Literal {
-        value: LiteralType::Num(15.0)
+        value: LiteralType::Num(15.0),
     });
     let l3 = Expr::Literal(Literal {
-        value: LiteralType::Num(9.0)
+        value: LiteralType::Num(9.0),
     });
     let b1 = Expr::Binary(Binary {
         left: Box::new(l1),
         operator: Token {
             r#type: TokenType::Star,
             lexme: "*".to_string(),
-            literal: None
+            literal: None,
         },
-        right: Box::new(l2)
+        right: Box::new(l2),
     });
     let g = Expr::Grouping(Grouping {
-        expression: Box::new(b1)
+        expression: Box::new(b1),
     });
     let b2 = Expr::Binary(Binary {
         left: Box::new(l3),
         operator: Token {
             r#type: TokenType::Plus,
             lexme: "+".to_string(),
-            literal: None
+            literal: None,
         },
-        right: Box::new(g)
+        right: Box::new(g),
     });
     assert_eq!("(+ 9 (group (* 4 15)))", b2.accept());
 }
